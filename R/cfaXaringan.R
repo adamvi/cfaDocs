047ff1391e1c393377f14e88fc87dab1e1aaf66b
Adam VanIwaarden <avaniwaarden@gmail.com>
2022-05-04 16:45:16 -0600

    Add new utils for making footer and GitHub OctoCat links more flexible

--- R/cfaXaringan.R
+++ R/cfaXaringan.R
@@ -1,20 +1,27 @@
 #' @title Xaringan output format for the Center for Assessment
 #'
-#' @description Function to render xaringan/remark.js presentation slides using custom templates and themes.
-#'   This output format produces an HTML file that contains the Markdown source (knitted from R Markdown) and
-#'   JavaScript code to render slides.
+#' @description Function to render xaringan/remark.js presentation slides using
+#' custom templates and themes. This output format produces an HTML file that contains
+#' the Markdown source (knitted from R Markdown) and JavaScript code to render slides.
 #'
-#' @param theme Name of CSS based theme for slides (length 1). Alternatively `NULL` with `css` argument specified Default: 'cfa-a'
-#' @param theme_copy Boolean - should the theme assets be copied to users xaringan built-in themes, Default: TRUE
-#' @param include features to include, such as custom CfA footer and a preset list of xaringanExtra add-ins. 
-#'   Default: list(footer = "default", xaringanExtra = "default", xExtra_use = c("share_again", "slide_tone", "tile_view", "clipboard"))
-#' @param css Optional file paths to css assets (`theme` must be NULL if used). Users can also provide a mix of css
-#'   from multiple themes using a specific file path convention, e.g. `c("**/cfa-a.css", "**/cfa-b-fonts.css")`. Default: NULL
-#' @param extras A named list of extra resources to be copied to the `lib_dir`, e.g. `list(fig = "cfa_assets/fig")` 
-#'   (or in Rmd YAML as `extras: !expr list(fig = 'cfa_assets/fig')` for figures placed in Rmd directly with <img> tag). Default: list()
-#' @param lib_dir Directory name of external/custom resources to be copied into (e.g. xaringanExtra js/css dependencies). Default: 'libs'
-#' @param asset_dir Directory name of custom resource location. Assumes subdirectories with names "css", "img", or as given in `extras`. Default: 'cfa_assets'
-#' @param ... Additional arguments passed to `xaringan::moon_reader`
+#' @param theme Name of CSS based theme for slides (length 1). Alternatively \code{NULL}
+#'     with \code{css} argument specified Default: 'cfa-a'
+#' @param theme_copy Boolean - should the theme assets be copied to users
+#'     xaringan built-in themes, Default: TRUE
+#' @param include features to include, such as custom CfA footer and a preset list of
+#'     xaringanExtra add-ins. Default: \code{list(footer = "default", xaringanExtra = "default",
+#'     xExtra_use = c("share_again", "slide_tone", "tile_view", "clipboard"))}
+#' @param css Optional file paths to css assets (\code{theme} must be NULL if used). Users can
+#'     also provide a mix of css from multiple themes using a specific file path convention,
+#'     e.g. \code{c("**/cfa-a.css", "**/cfa-b-fonts.css")}. Default: NULL
+#' @param extras A named list of extra resources to be copied to the \code{lib_dir}, e.g.
+#'     \code{list(fig = "cfa_assets/fig")} (or in Rmd YAML as \code{extras: !expr list(fig = 'cfa_assets/fig')}
+#'     for figures placed in Rmd directly with <img> tag). Default: list()
+#' @param lib_dir Directory name of external/custom resources to be copied into
+#'     (e.g. xaringanExtra js/css dependencies). Default: 'libs'
+#' @param asset_dir Directory name of custom resource location. Assumes subdirectories
+#'     with names "css", "img", or as given in \code{extras}. Default: 'cfa_assets'
+#' @param ... Additional arguments passed to \code{\link[xaringan]{moon_reader}}
 #' @return HTML slide presentation.
 #' @details Slides are formatted with any combination of CfA based themes
 #'
@@ -35,7 +42,10 @@
 cfaXaringan <- function(
   theme = "cfa-a",
   theme_copy = TRUE,
-  include = list(footer = "default", xaringanExtra = "default", xExtra_use = c("share_again", "slide_tone", "tile_view", "clipboard")),
+  include = list(
+    footer = "default",
+    xaringanExtra = "default",
+    xExtra_use = c("share_again", "slide_tone", "tile_view", "clipboard")),
   css = NULL,
   extras = list(),
   lib_dir = "libs",
@@ -56,6 +66,7 @@ cfaXaringan <- function(
       copyResources(resources=theme.css)
       theme.css <- gsub("[.](?:sa|sc|c)ss$", "", theme.css)
     } else {
+      options(htmltools.dir.version = FALSE)
       theme_css <- if (length(theme.css)) {
         if (is.null(check_builtin_css(theme))) {
           htmltools::htmlDependency(
@@ -73,23 +84,24 @@ cfaXaringan <- function(
     }
   } else {
     # allow user to mix and match cfa theme assets
+    options(htmltools.dir.version = FALSE)
     theme.css <- gsub("^\\*\\*", pkg_resource("css"), css)
     theme_css <- htmltools::htmlDependency(
       "css", "0.0.1", dirname(theme.css),
       stylesheet = basename(theme.css),
       all_files = FALSE
     )
-    if (!dir.exists(new_dir <- file.path(lib_dir, asset_dir, "css"))) 
+    if (!dir.exists(new_dir <- file.path(lib_dir, asset_dir, "css")))
       dir.create(new_dir, recursive = TRUE)
     css.cp <- htmltools::copyDependencyToDir(theme_css, file.path(lib_dir, asset_dir), mustWork = TRUE)
     theme.css <- file.path(new_dir, basename(theme.css))
   }
 
-  #  Things like figures placed directly with <img>, assets without a http url (local only), etc. 
+  #  Things like figures placed directly with <img>, assets without a http url (local only), etc.
   if (length(extras)) {
     for (asset in seq_len(length(extras))) {
       tmp_dir <- extras[[asset]]
-      if (!dir.exists(new_dir <- file.path(lib_dir, asset_dir, names(extras)[asset]))) 
+      if (!dir.exists(new_dir <- file.path(lib_dir, asset_dir, names(extras)[asset])))
         dir.create(new_dir, recursive = TRUE)
       tmp.cp <- file.copy(list.files(tmp_dir, full.names = TRUE), new_dir, overwrite = TRUE)
     }
@@ -97,16 +109,35 @@ cfaXaringan <- function(
 
   if (length(include)) {
     if (length(include[["footer"]])) {
-      if (include[["footer"]] == "default")
-        include[["footer"]] <- pkg_resource("rmd/cfa-footer.md")
+      if (include[["footer"]] == "default") {
+        foot_md <- makeFooter(ft.text = include[["footer_text"]], lib = lib_dir)
+      } else {
+        foot_md <- makeFooter(ft.file = include[["footer"]],
+                              ft.text = include[["footer_text"]], lib = lib_dir)
+      }
     }
     if (length(include[["xaringanExtra"]])) {
       if (include[["xaringanExtra"]] == "default")
         include[["xaringanExtra"]] <- pkg_resource("rmd/cfa-xaringanExtra.Rmd")
       if (!dir.exists(tmp_libdir <- file.path(dirname(include[["xaringanExtra"]]), lib_dir)))
         dir.create(tmp_libdir, recursive = TRUE)
-      head_html <- file.path(lib_dir, "add_in_header.html")
+      head_html <- file.path(lib_dir, "in_header.html")
       params <- list(xExtra_use = include[["xExtra_use"]])
+      if (!is.null(theme)) {
+        if (theme == "cfa-a") {
+          params[["width"]] <- "150px"
+          params[["height"]] <- "100px"
+          params[["top"]] <- "1em"
+          params[["right"]] <- "1.25em"
+          params[["img"]] <- "https://raw.githubusercontent.com/CenterForAssessment/cfaDocs/main/inst/rmarkdown/templates/xaringan/resources/img/nciea-logo-long.svg"
+        }
+      } else {
+        params[["width"]] <- "86px"
+        params[["height"]] <- "100px"
+        params[["top"]] <- "0.5em"
+        params[["right"]] <- "0.5em"
+        params[["img"]] <- "https://raw.githubusercontent.com/CenterForAssessment/cfaDocs/main/inst/rmarkdown/templates/xaringan/resources/img/CFA_Logo.svg" # could be local if copied to `libs`
+      }
       tmp.xE <- rmarkdown::render(include[["xaringanExtra"]], output_file = head_html)
       R.utils::copyDirectory(tmp_libdir, lib_dir)
       unlink(tmp_libdir, recursive = TRUE)
@@ -116,10 +147,10 @@ cfaXaringan <- function(
       tmp_html <- c("<link rel=\"icon\" type=\"image/png\" href=\"https://centerforassessment.github.io/assets/favicon.png\" sizes=\"16x16\">", tmp_html)
       writeLines(tmp_html, head_html)
     }
-  }
+  } else foot_md <- NULL
 
   if (!exists("head_html")) {
-    head_html <- file.path(lib_dir, "add_in_header.html")
+    head_html <- file.path(lib_dir, "in_header.html")
     writeLines("<link rel=\"icon\" type=\"image/png\" href=\"https://centerforassessment.github.io/assets/favicon.png\" sizes=\"16x16\">", head_html)
   }
 
@@ -127,7 +158,7 @@ cfaXaringan <- function(
   xaringan::moon_reader(
     css = theme.css,
     lib_dir = lib_dir,
-    includes = rmarkdown::includes(in_header = head_html, before_body = include[["footer"]]),
+    includes = rmarkdown::includes(in_header = head_html, before_body = foot_md),
     ...
   )
 }
--- R/utils.R
+++ R/utils.R
@@ -5,16 +5,13 @@
 pkg_resource <- function(...) {
   system.file(
     "rmarkdown", "templates", "xaringan", "resources", ...,
-    package = "cfaDocs", mustWork = TRUE
-  )
+    package = "cfaDocs", mustWork = TRUE)
 }
-
 
 xrgn_resource <- function(...) {
   system.file(
     "rmarkdown", "templates", "xaringan", "resources", ...,
-    package = "xaringan", mustWork = TRUE
-  )
+    package = "xaringan", mustWork = TRUE)
 }
 
 
@@ -22,7 +19,6 @@ list_css <- function() {
   css <- list.files(pkg_resource("css"), "[.]css$", full.names = TRUE)
   stats::setNames(css, gsub(".css$", "", basename(css)))
 }
-
 
 check_builtin_css <- function(theme) {
   valid <- names(list_css())
@@ -34,11 +30,32 @@ check_builtin_css <- function(theme) {
   hint <- if (is.na(maybe)) "" else paste0('; did you mean "', maybe, '"?')
   stop(
     '"', invalid, '" is not a valid cfaDocs theme', if (hint != "") hint else ".",
-    " Use `cfaDocs:::list_css()` to view all built-in themes.", call. = FALSE
-  )
+    " Use `cfaDocs:::list_css()` to view all built-in themes.", call. = FALSE)
 }
 
 
 copyResources <- function(type="css", resources, ...) {
   file.copy(file.path(pkg_resource(type), resources), xrgn_resource(), overwrite = TRUE)
+}
+
+makeFooter <- function(ft.file=pkg_resource("rmd/cfa-footer.md"), ft.text=NULL, lib) {
+  tmp_foot <- readLines(ft.file)
+  if (is.null(ft.text)) {
+    ft.text <- "<a class=\"footer\" href=\"http://www.nciea.org\">     www.nciea.org</a>"
+  } else {
+    if (!grepl("^<a class=\"footer\"", ft.text))
+      ft.text <- paste0("<a class=\"footer\">", ft.text, "</a>")
+  }
+  tmp_foot <- gsub("TMP_FOOTER", ft.text, tmp_foot)
+  ft.file <- file.path(lib, "footer.md")
+  writeLines(c("layout: true", "", tmp_foot, "", "---", ""), ft.file)
+  ft.file
+}
+
+makeOctoCat <- function(
+  oc.file = pkg_resource("rmd/cfa-octocat-corner.md"),
+  link = "https://github.com/CenterForAssessment") {
+    tmp_octcat <- readLines(oc.file, warn=FALSE)
+    tmp_octcat <- sub("TMP_LINK", link, tmp_octcat)
+    tmp_octcat
 }
