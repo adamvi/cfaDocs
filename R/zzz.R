c63383c77e616284fa370c2c061b14b8e3607569
Adam VanIwaarden <avaniwaarden@gmail.com>
2022-05-05 16:57:30 -0600

    Bump version and changes for R CMD check

--- R/zzz.R
+++ R/zzz.R
@@ -7,6 +7,6 @@
       packageStartupMessage(crayon::magenta$bold('cfaDocs',
         paste(paste0(unlist(strsplit(as.character(utils::packageVersion("cfaDocs")), "[.]")),
         c(".", "-", ".", "")), collapse = ""),
-        ' (5-4-2022). For help visit https://centerforassessment.github.io/cfaDocs'))
+        ' (5-5-2022). For help visit https://centerforassessment.github.io/cfaDocs'))
   }
 }
