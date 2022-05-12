b091dfc9ab5909bea85d7c7e7d8f04615466c9b2
Adam VanIwaarden <avaniwaarden@gmail.com>
2022-05-12 07:09:53 -0600

    Add in w_paper_paged function and associated templates, assets, etc.

---
+++ R/w_paper_paged.R
@@ -0,0 +1,103 @@
+#' @title Working/white paper output format
+#'
+#' @description This is a basic output format appropriate for report drafts,
+#' working/white papers, etc. and can also incorporate appendices. This template
+#' is based on the work done by Paul Bauer and Camille Landesvatter. See their
+#' \href{https://github.com/paulcbauer/Writing_a_reproducable_paper_in_pagedown}{GitHub repository}
+#' and the description of \code{pagedown::\link[pagedown]{jss_paged}} for
+#' more general information and context.
+#'
+#' @author Adam Van Iwaarden
+#'
+#' @param ... Arguments passed to \code{bookdown::\link[bookdown]{html_document2}}
+#'   and/or \code{rmarkdown::\link[rmarkdown]{html_document_base}}.
+#' @param template The path to the Pandoc template to convert Markdown to HTML.
+#'   The \code{'default'} will use the 'wp_paged.html' template from this package.
+#' @param css A character vector of CSS and Sass file paths. If a path
+#'   does not contain the \file{.css}, \file{.sass}, or \file{.scss} extension,
+#'   it is assumed to be a built-in CSS file. The package \code{'default'}
+#'   are \code{c('wp-page.css', 'wp-default.css', 'wp-fonts.css')}.
+#' @param csl The path of the Citation Style Language (CSL) file used to format
+#'   citations and references (see the
+#'   \href{https://pandoc.org/MANUAL.html#citations}{Pandoc documentation}). The
+#'   \code{'default'} is the 'american-sociological-association.csl' as used by
+#'   Bauer and Landesvatter (see description).
+#' @param number_sections Boolean whether to number section headers: if 'TRUE',
+#'   figure/table numbers will be of the form 'X.i', where 'X' is the current
+#'   first-level number, and 'i' is an incremental number (the i-th figure/table);
+#'   if 'FALSE', section figures/tables will be numbered sequentially in the document
+#'   from 1, 2, ..., and you cannot cross-reference section headers in this case.
+#' @param self_contained Produce a standalone HTML file with no external dependencies,
+#'   using data: URIs to incorporate the contents of linked scripts, stylesheets,
+#'   images, and videos. Note that even for self contained documents MathJax is
+#'   still loaded externally (this is necessary because of its size). Default: TRUE
+#' @param anchor_sections to show section anchors when mouse hovers for all headers.
+#'   A list can also be passed with 'style' and/or 'depth' to customize the behavior.
+#'   See Anchor Sections Customization section., Default: FALSE
+#' @param mathjax Include mathjax. Passing \code{'default'} uses an https URL from
+#'   a MathJax CDN. The 'local' option uses a local version of MathJax (which is
+#'   copied into the output directory). You can pass an alternate URL or pass
+#'   \code{NULL} to exclude MathJax entirely. Default: NULL
+#' @param pandoc_args Additional command line options to pass to pandoc, Default: NULL
+#' @return An R Markdown output format.
+#' @seealso
+#'  \code{\link[pagedown]{jss_paged}}
+#'  \code{\link[bookdown]{html_document2}}
+#'  \code{\link[rmarkdown]{html_document_base}}
+#' @references \url{https://pagedown.rbind.io}
+#' @rdname w_paper_paged
+#' @import pagedown
+#' @importFrom htmltools htmlDependency tagList
+#' @importFrom utils packageVersion 
+#' @export
+
+w_paper_paged = function(
+  ...,
+  template = "default",
+  css = "default",
+  csl = "default",
+  number_sections = TRUE,
+  self_contained = TRUE,
+  anchor_sections = FALSE,
+  mathjax = NULL,
+  pandoc_args = NULL
+) {
+
+  if (tolower(template) == "default")
+    template <- cfa_paged_res("w_paper", "pandoc", "wp_paged.html")
+
+  if (tolower(css) == "default")
+    css <- file.path("rmarkdown/templates/pagedown/w_paper/css",
+                     list.files(cfa_paged_res("w_paper", "css")))
+
+  if (tolower(csl) == "default")
+    csl <- cfa_paged_res("w_paper", "csl", "american-sociological-association.csl")
+
+  extra_deps <- htmltools::tagList(dep_css(css))
+
+  wp_format = pagedown:::html_format(
+    ...,
+    template = template, css = NULL, csl = csl, number_sections = number_sections,
+    self_contained = self_contained, anchor_sections = anchor_sections, 
+    mathjax = mathjax, theme = NULL, highlight = NULL,
+    .pagedjs = TRUE, .pandoc_args = c(
+      pagedown:::lua_filters("uri-to-fn.lua", "footnotes.lua", "jss.lua"),
+      "--lua-filter", cfa_paged_res("w_paper", "lua", "multiple-bibliographies.lua"),
+      if (!is.null(csl)) c("--csl", csl),
+      "--metadata", "link-citations=true",
+      pandoc_args
+    ),
+    .dependencies = extra_deps
+  )
+
+  opts_wp = list(
+    prompt = TRUE, comment = NA, R.options = list(prompt = 'R> ', continue = 'R+ '),
+    fig.align = 'center', fig.width = 4.9, fig.height = 3.675,
+    class.source = 'r-chunk-code'
+  )
+  for (i in names(opts_wp)) {
+    wp_format$knitr$opts_chunk[[i]] = opts_wp[[i]]
+  }
+
+  wp_format
+}
