bf2e66c12efab672793505622caaedcdfbc73d77
Adam VanIwaarden <avaniwaarden@gmail.com>
2023-06-24 21:30:31 -0600

    Bump version

--- R/zzz.R
+++ R/zzz.R
@@ -7,6 +7,6 @@
       packageStartupMessage(crayon::magenta$bold("cfaDocs",
         paste(paste0(unlist(strsplit(as.character(utils::packageVersion("cfaDocs")), "[.]")),
         c(".", "-", ".", "")), collapse = ""),
-        " (11-11-2022). For help visit https://centerforassessment.github.io/cfaDocs"))
+        " (5-30-2023). For help visit https://centerforassessment.github.io/cfaDocs"))
   }
 }
