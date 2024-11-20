def47bb2fe89533355f0c2fadfeb483ae7214466
Adam VanIwaarden <avaniwaarden@gmail.com>
2024-11-20 13:42:46 -0700

    Changes after most of 2024 tech reports

--- R/zzz.R
+++ R/zzz.R
@@ -7,6 +7,6 @@
       packageStartupMessage(crayon::magenta$bold("cfaDocs",
         paste(paste0(unlist(strsplit(as.character(utils::packageVersion("cfaDocs")), "[.]")),
         c(".", "-", ".", "")), collapse = ""),
-        " (9-5-2024). For help visit https://centerforassessment.github.io/cfaDocs"))
+        " (11-20-2024). For help visit https://centerforassessment.github.io/cfaDocs"))
   }
 }
